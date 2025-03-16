5ff50b9defde9a15c4ff276624e80d09abbaa824
Tajul Islam <tajul.islam@portonics.com>
2025-03-17 00:37:17 +0600

    feat(auth): added database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExplorerModule } from './explorer/explorer.module';
 import { FileStorageModule } from './file-storage/file-storage.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
     }),
     ExplorerModule,
     FileStorageModule,
