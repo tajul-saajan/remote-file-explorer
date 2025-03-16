cc3020e222aed149a1d8e00176662a6c4bc9dc90
Tajul Islam <tajul.islam@portonics.com>
2025-03-17 01:02:54 +0600

    feat(auth): added user service

---
+++ src/user/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async findOne(username: string) {
+    return await this.userRepository.findOne({ where: { username } });
+  }
+
+  async create(user: Partial<User>): Promise<User> {
+    const newUser = this.userRepository.create(user);
+    return this.userRepository.save(newUser);
+  }
+}
